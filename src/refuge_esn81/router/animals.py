2ab18379f45e1dea92733453890d568c81531b63
Alexis <alexiscristiano2004@gmail.com>
2025-11-13 14:19:07 +0100

    fichier router animals

--- src/refuge_esn81/router/animals.py
+++ src/refuge_esn81/router/animals.py
@@ -1,5 +1,17 @@
 from fastapi import APIRouter, FastAPI, Depends, HTTPException
-
+from refuge_esn81.schemas.animalSchema import Animal, AnimalCreate
+from sqlalchemy.orm import Session
+from refuge_esn81.database.database import get_db
+from refuge_esn81.services.animalService import AnimalService
 
 animalsRouter = APIRouter(prefix="/animals", tags=["animals"])
 
+@animalsRouter.get("/", response_model=list[Animal])
+async def get_species(db: Session = Depends(get_db)):
+    service = AnimalService()
+    return service.get_animals(db)
+
+@animalsRouter.post("/", response_model=Animal)
+def create_new_species(specie: AnimalCreate, db: Session = Depends(get_db)):
+    service = AnimalService()
+    return service.create_animal(db, specie)
